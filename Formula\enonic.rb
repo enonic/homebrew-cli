498dec96bab05300729e7b37a6eff4e59b4aa1e2
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-08 16:04:11 +0300

    Brew formula update for enonic version 0.1.5

--- Formula\enonic.rb
+++ Formula\enonic.rb
@@ -1,9 +1,9 @@
 class Enonic < Formula
   desc "Enonic XP command line interface."
   homepage "http://enonic.com/"
-  url "https://github.com/enonic/xp-cli-scoop/releases/download/0.1.4/enonic_0.1.4_Mac_64-bit.tar.gz"
-  version "0.1.4"
-  sha256 "0ae05781960c5e8c9abf8ac0efb4417b4a665dfb66db4836138adca3f92ff230"
+  url "https://github.com/enonic/xp-cli-scoop/releases/download/0.1.5/enonic_0.1.5_Mac_64-bit.tar.gz"
+  version "0.1.5"
+  sha256 "3c94da222392d98850f3fcdce69b8f3b0f674b7c1cf8d1cdc8e0ee3a1b7f593c"
 
   def install
     bin.install "enonic"
