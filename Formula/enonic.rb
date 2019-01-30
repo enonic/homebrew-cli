6cc822961bad97fc3ebab0b411cf2a0d84d25467
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-30 18:06:35 +0300

    Brew formula update for enonic version 0.2.2

--- Formula/enonic.rb
+++ Formula/enonic.rb
@@ -3,7 +3,7 @@ class Enonic < Formula
   homepage "http://enonic.com/"
   url "http://repo.enonic.com/public/com/enonic/cli/enonic/0.2.2/enonic_0.2.2_Mac_64-bit.tar.gz"
   version "0.2.2"
-  sha256 "dde4147bb6434d33b39288da8de93bfcbdf30c62e33ed22f3d726e9d9fd587e2"
+  sha256 "5bcb4157bacfe3d3056ac02c9e4eeb013612adee3222daa835412ed8a9df1cd7"
 
   def install
     bin.install "enonic"
