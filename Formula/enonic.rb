cfd9d933e8bb798ea06cc3c004423c298727f921
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-03 14:57:53 +0100

    Brew formula update for enonic version 1.1.0

--- Formula/enonic.rb
+++ Formula/enonic.rb
@@ -2,16 +2,16 @@
 class Enonic < Formula
   desc "Command-line interface for Enonic XP"
   homepage "https://enonic.com/"
-  version "1.0.12"
+  version "1.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://repo.enonic.com/public/com/enonic/cli/enonic/1.0.12/enonic_1.0.12_Mac_64-bit.tar.gz"
-    sha256 "25847bceebeab22c9745e590711d719a1980a0ebfc45ec1c5835b36634272552"
+    url "https://repo.enonic.com/public/com/enonic/cli/enonic/1.1.0/enonic_1.1.0_Mac_64-bit.tar.gz"
+    sha256 "657e50387b808e266d1f25060148b84a37f0a639d76672df3d5560fe9c4e9b8b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://repo.enonic.com/public/com/enonic/cli/enonic/1.0.12/enonic_1.0.12_Linux_64-bit.tar.gz"
-      sha256 "b0694b476ac3a22221f7e6156e2f5a6d8434823755dfdc06e76a3ad217fc2320"
+      url "https://repo.enonic.com/public/com/enonic/cli/enonic/1.1.0/enonic_1.1.0_Linux_64-bit.tar.gz"
+      sha256 "c9cef10359f5107c145852d012c50aab38d55d1fcdda05fd100a304b68d7eb11"
     end
   end
 
